e52a68d31a5f1df464f47f02595e7e1e959f4284
Hassan Mahmood <85311173+HassanMahmood001@users.noreply.github.com>
2022-05-29 18:51:36 +0500

    Update mp4-to-mp3.py

--- mp4-to-mp3.py
+++ mp4-to-mp3.py
@@ -12,13 +12,10 @@ Tk().withdraw()
 path2 = filedialog.askdirectory()
 path2=path2+"/"
 
-# path='E:/Kpop/2/'
-# path2=r'E:/mp3kpop/2/'
+
 list = os.listdir(path)
 
 for f in list:
-    # print(path+f)
-    # ff,fg=f.split(".")
     new=path2+f+".mp3"
     # print(new)
     try:
